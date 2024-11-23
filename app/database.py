1bae90c0f3fcc2756ea30e5fbde18e718ceb4928
4319 Задорожный А.В <zvvqwerty1@gmail.com>
2024-11-23 22:24:14 +0300

    Фикс ошибок2

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.config import settings
 
-async_engine = create_async_engine(url=settings.DATABASE_URL)
+async_engine = create_async_engine(url=settings.DATABASE_URL, echo=True)
 
 async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False)
 
