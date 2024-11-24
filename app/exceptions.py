52c8a18fbf0d556117a043ceec07c57675b0ee3f
4319 Задорожный А.В <zvvqwerty1@gmail.com>
2024-11-24 23:28:41 +0300

    Выношу ошибки

---
+++ app/exceptions.py
@@ -0,0 +1,3 @@
+from fastapi import HTTPException, status
+EmailIsBusy = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Почта уже занята!")
+AuthenticationError = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Ошибка авторизации")
--- app/users/services.py
+++ app/users/services.py
@@ -1,8 +1,9 @@
 
-from fastapi import HTTPException, status
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.exceptions import EmailIsBusy, AuthenticationError
 from app.users.auth import create_access_token
 from app.users.models import Users
 from app.users.schemas import UserCreate, UserResponse, UserLogin
@@ -14,7 +15,7 @@ async def register_user_service(user:UserCreate,db : AsyncSession) -> UserRespon
     result = await db.execute(res)
     existing_user = result.mappings().first()
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Почта уже занята!")
+        raise EmailIsBusy
     hashed_password = hash_password(user.password)
     new_user = Users(name=user.name, email=user.email, hashed_password=hashed_password)
     db.add(new_user)
@@ -28,6 +29,6 @@ async def login_user_service(user: UserLogin, db: AsyncSession) -> dict:
     db_user = result.scalars().first()
     print(db_user)
     if not db_user or not verify_password(user.password, db_user.hashed_password):
-        raise HTTPException(status_code=401, detail="Ошибка авторизации")
+        raise AuthenticationError
     access_token = create_access_token(data={"sub": db_user.email})
     return {"access_token": access_token, "token_type": "bearer"}
