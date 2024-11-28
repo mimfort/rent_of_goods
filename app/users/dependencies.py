9498cfdde500cd7580da7778fb32ac7558be87f5
4319 Задорожный А.В <zvvqwerty1@gmail.com>
2024-11-28 13:53:48 +0300

    fix get_token (убрал async)

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -1,7 +1,7 @@
 from app.exceptions import TokenAbsentException
 from fastapi import Request
 
-async def get_token(request: Request):
+def get_token(request: Request):
     token = request.cookies.get("access_token")
     if not token:
         raise TokenAbsentException
