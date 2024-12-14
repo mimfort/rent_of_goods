4dd73ac26222dbc3285f43a687ce0f4fb4a1a9dc
4319 Задорожный А.В <zvvqwerty1@gmail.com>
2024-12-14 15:54:38 +0300

    Проверку jwt токена

--- app/categories/router.py
+++ app/categories/router.py
@@ -1,8 +1,12 @@
 from fastapi import HTTPException
 
 from fastapi import APIRouter
+from fastapi.params import Depends
 
 from app.categories.dao import CategoriesDAO
+from app.exceptions import TokenAbsentException
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 
 router = APIRouter(
     prefix="/categories",
@@ -17,9 +21,10 @@ async def get_category_by_id(category_id: int):
     return category
 
 @router.post("/add_category")
-async def add_category(name: str, description: str = ''):
+async def add_category(name: str, description: str = '',user: Users = Depends(get_current_user)):
+    if not user:
+        raise TokenAbsentException
     res = await CategoriesDAO.add(name=name, description=description)
-    
     return res
 
 @router.post("/update_category/{category_id}")
